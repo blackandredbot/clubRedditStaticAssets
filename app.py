bc9e89e2275bf1235f6ae53dacf00497dd9751a2
Michael Foley <folem@amazon.com>
2023-03-27 20:37:56 -0400

    Correcting namespace conflicts

--- app.py
+++ app.py
@@ -11,21 +11,11 @@ app = cdk.App()
 default_env = cdk.Environment(account="363951782376", region="us-east-1")
 application_prefix = "ClubRedditStaticAssets"
 
-
 ClubRedditStaticAssetsStack(
     app,
     f"{application_prefix}Stack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-    # env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
     env=default_env,
     application_prefix=application_prefix
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
 )
 
 app.synth()
